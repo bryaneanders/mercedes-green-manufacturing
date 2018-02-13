5af9a983f100f382831e00d396bffba827108a8b
Bryan Anders <bryaneanders@gmail.com>
2018-02-13 08:18:06 -0800

    Init and data check in

---
+++ mercedes-testing/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+# initial __init__.py
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 07:41:52 2018
+
+@author: Bryan Anders
+"""
+
+from distutils.core import setup
+
+setup(name='Mercedes Benz Effeciency Testing',
+      version='1.0',
+      description='An attempt at solving the Mercedes-Benz Greener Manufactruing challenge on Kaggle. This is my first data science project.',
+      author='Bryan Anders',
+      author_email='bryaneanders@gmail.com',
+      url='https://github.com/bryaneanders/mercedes-green-manufacturing',
+      packages=['mercedes-testing']
+      )
